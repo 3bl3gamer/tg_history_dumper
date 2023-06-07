d9b00c977759a849cd4eea2e321dbdd0225b7e46
3bl3gamer <3bl3gamer@gmail.com>
2023-06-07 09:45:17 +0300

    A bit better SaveFileCallbackFunc

--- main.go
+++ main.go
@@ -283,22 +283,26 @@ func dump() error {
 
 	saver := &JSONFilesHistorySaver{Dirpath: config.OutDirPath}
 	saver.SetFileRequestCallback(func(chat *Chat, file *TGFileInfo, msgID int32) error {
-		var err error
 		if config.Media.Match(chat, file) == MatchTrue {
 			fpath, err := saver.MessageFileFPath(chat, msgID, file.FName)
+			if err != nil {
+				return merry.Wrap(err)
+			}
 			_, err = os.Stat(fpath)
 			if os.IsNotExist(err) {
 				log.Info("downloading file to %s", fpath)
-				_, err = tg.DownloadFileToPath(fpath, file.InputLocation, file.DcID, int64(file.Size), NewFileProgressLogger())
+				_, err := tg.DownloadFileToPath(fpath, file.InputLocation, file.DcID, int64(file.Size), NewFileProgressLogger())
 				if isBrokenFileError(err) {
 					log.Error(nil, "in chat %d %s (%s): wrong file: %s", chat.ID, chat.Title, chat.Username, fpath)
 					err = nil
 				}
+				return merry.Wrap(err)
 			}
+			return merry.Wrap(err)
 		} else {
 			log.Debug("skipping file '%s' of message #%d", file.FName, msgID)
+			return nil
 		}
-		return merry.Wrap(err)
 	})
 
 	// loading chats
